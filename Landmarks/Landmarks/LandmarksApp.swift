7b82c554c4255a37141b8bca9a08a0005f7ef8b4
LowSpecx <mauricetin.916@gmail.com>
2022-06-21 11:42:19 +0700

    first commit

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Maurice Tin on 20/06/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
